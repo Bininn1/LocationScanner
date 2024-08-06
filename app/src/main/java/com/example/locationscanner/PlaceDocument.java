21f3a58db31601ce8c481e8f2614d2a47476454d
Rachel <cofla226@naver.com>
2024-08-07 03:40:35 +0900

    remove: sensitive data and update gitignore

--- app/src/main/java/com/example/locationscanner/PlaceDocument.java
+++ app/src/main/java/com/example/locationscanner/PlaceDocument.java
@@ -5,7 +5,6 @@ import com.google.gson.annotations.SerializedName;
 public class PlaceDocument {
     @SerializedName("place_name")
     private String placeName;
-    // 필요한 다른 필드를 포함시키세요
 
     public String getPlaceName() {
         return placeName;
